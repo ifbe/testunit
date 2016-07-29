8dab1866ef8c949801c020a89a879269ce036205
ifbe <ifbe@live.cn>
2016-07-29 09:19:42 +0800

    new repo for codetree

--- software1/codetree/bugsample/has_include.c
+++
@@ -1,6 +0,0 @@
-#if __has_include("stdint.h)
-#endif
-
-// expected-error@+1 {{expected "FILENAME" or <FILENAME>}} expected-warning@+1 {{missing terminating '"' character}} expected-error@+1 {{token is not a valid binary operator in a preprocessor subexpression}}
-#if __has_include(stdint.h")
-#endif
--- software1/codetree/bugsample/rme32.c
+++
@@ -1,9 +0,0 @@
-#define RME32_WCR_START     (1 << 0)    /* startbit */
-#define RME32_WCR_MONO      (1 << 1)    /* 0=stereo, 1=mono
-                                           Setting the whole card to mono
-                                           doesn't seem to be very useful.
-                                           A software-solution can handle 
-                                           full-duplex with one direction in
-                                           stereo and the other way in mono. 
-                                           So, the hardware should work all 
-                                           the time in stereo! */
