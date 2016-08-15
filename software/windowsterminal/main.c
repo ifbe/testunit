df204eb2d555821a29e09a1b04e90a224de9c213
ifbe <ifbe@live.cn>
2016-08-15 23:07:39 +0800

    modified:   software/windowsterminal/main.c

--- software/windowsterminal/main.c
+++ software/windowsterminal/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <windows.h>
+//
+static HANDLE h;
+//
 static char tab[0x100000];
 
 
@@ -8,16 +11,21 @@ static char tab[0x100000];
 void gotoxy(int x,int y)
 {
 	COORD pos = {x,y};
-	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleCursorPosition(h,pos);
 }
 int main (void)
 {
 	char ch;
 	int x,y;
-	int width=80,height=25;
+	int width,height;
+	CONSOLE_SCREEN_BUFFER_INFO bInfo;
 
-	ch=0;
+	h = GetStdHandle(STD_OUTPUT_HANDLE);
+	GetConsoleScreenBufferInfo(h, &bInfo );
+	width = bInfo.srWindow.Right - bInfo.srWindow.Left + 1;
+	height = bInfo.srWindow.Bottom - bInfo.srWindow.Top + 1;
+
+	ch = 0;
 	while(1)
 	{
 		for(y=0;y<height;y++)
@@ -33,7 +41,7 @@ int main (void)
 		printf("%s",tab);
 		fflush(stdout);
 
-		sleep(1);
+		Sleep(1);
 		ch = (ch+1)%0x50;
 	}
 }
