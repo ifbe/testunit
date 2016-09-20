145a82b987f8d9a770888731fe41670b42fc953b
ifbe <ifbe@live.cn>
2016-09-20 17:07:05 +0800

    haha

--- software1/codetree/bugsample/cpumap.c
+++
@@ -1,5 +0,0 @@
-int cpu_map__build_map(struct cpu_map *cpus, struct cpu_map **res,
-                       int2 (*f)(struct cpu_map *map, int3 cpu, void *data),
-                       void *data)
-{
-}
--- software1/codetree/bugsample/ole.h
+++
@@ -1,4 +0,0 @@
-#ifdef _OLE2_H_
-#error You can't use ole.h and ole2.h together.
-#endif
-
