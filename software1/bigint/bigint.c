46cc4aecafded468e505362f7be5ffb7f7412f59
ifbe <ifbe@live.cn>
2016-11-21 14:33:30 +0800

    修改：     software1/bigint/bigint.c

--- software1/bigint/bigint.c
+++ software1/bigint/bigint.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 #define u8 unsigned char
+#define u64 unsigned long long
 int bigadd(u8* abuf, int alen, u8* bbuf, int blen, u8* answer, int max)
 {
 	int j;
@@ -122,40 +123,120 @@ int bigsub(u8* abuf, int alen, u8* bbuf, int blen, u8* answer, int max)
 	}
 	return j;
 }
+int bigmul_onebyte(
+        u8* abuf, int alen,
+        u8* bbuf, int index,
+        u8* res, int max)
+{
+        int j;
+        int temp = 0;
+
+        for(j=0;j<index;j++)res[j] = 0;
+        res += index;
+
+        for(j=0;j<alen;j++)
+        {
+                temp += abuf[j] * bbuf[index];
+		//printf("%x\n",temp);
+
+                res[j] = temp & 0xff;
+                temp >>= 8;
+        }
+	if(temp != 0)
+	{
+		res[alen] = temp;
+		j++;
+	}
+	return j+index;
+}
+int bigmul(
+        u8* abuf, int alen,
+        u8* bbuf, int blen,
+        u8* answer, int max,
+        u8* temp, int rsvd)
+{
+	int j;
+	int ret;
+	for(j=0;j<alen+blen;j++)answer[j] = 0;
+
+	for(j=0;j<blen;j++)
+	{
+		ret = bigmul_onebyte(
+			abuf, alen,
+			bbuf, j,
+			temp, 66666666
+		);
+
+		bigadd(answer, 66666666, temp, ret, answer, 6666666);
+/*
+		printf("%016llx%016llx\n%016llx%016llx\n\n",
+			*(u64*)(temp+8), *(u64*)temp,
+			*(u64*)(answer+8), *(u64*)answer
+		);
+*/
+	}
+}
 void main()
 {
 	int j;
 	int ret;
-	u8 answer[200]={0};
-	u8 a[11];
-	u8 b[10];
-	for(j=0;j<10;j++)
+	u8 a[16];
+	u8 b[16];
+	u8 c[16];
+	u8 d[16];
+	for(j=0;j<16;j++)
 	{
 		a[j] = 0xc0+j;
 		b[j] = 0xcf-j;
+		c[j] = d[j] = 0;
 	}
-	a[10] = 0;
 
 	//origin
-	for(j=0;j<sizeof(a);j++)
-	{
-		printf("%2x ",a[j]);
-	}
-	printf("\n");
+	printf("origin\n");
+	printf("	%016llx%016llx\n	%016llx%016llx\n",
+		*(u64*)(a+8),
+		*(u64*)a,
+		*(u64*)(b+8),
+		*(u64*)b
+	);
 
 	//add
-	ret = bigadd(a, 11, b, 10, a, 100);
-	for(j=0;j<sizeof(a);j++)
-	{
-		printf("%2x ",a[j]);
-	}
-	printf("\n");
+	ret = bigadd(a, 8, b, 8, c, 9);
+	printf("%llx + %llx\n",
+		*(u64*)a,
+		*(u64*)b
+	);
+	printf("	%016llx%016llx\n	%016llx%016llx\n",
+		(u64)0,
+		(*(u64*)a) + (*(u64*)b),
+		*(u64*)(c+8),
+		*(u64*)c
+	);
 
 	//sub
-	ret = bigsub(a, 11, b, 10, a, 100);
-	for(j=0;j<sizeof(a);j++)
-	{
-		printf("%2x ",a[j]);
-	}
-	printf("\n");
+	ret = bigsub(a, 8, b, 8, c, 9);
+	printf("%llx - %llx\n",
+		*(u64*)a,
+		*(u64*)b
+	);
+	printf("	%016llx%016llx\n	%016llx%016llx\n",
+		(u64)0,
+		(*(u64*)a) - (*(u64*)b),
+		*(u64*)(c+8),
+		*(u64*)c
+	);
+
+	//mul1
+	//ret = bigmul_onebyte(a, 8, b, 0, c, 100);
+	ret = bigmul(a, 8, b, 8, c, 16, d, 16);
+	printf("%llx * %llx\n",
+		*(u64*)a,
+		*(u64*)b
+	);
+	printf("	%016llx%016llx\n	%016llx%016llx\n",
+		(u64)0,
+		(*(u64*)a) * (*(u64*)b),
+		*(u64*)(c+8),
+		*(u64*)c
+	);
 }
