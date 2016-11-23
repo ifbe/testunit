ad617a3ccf14d44b97b7e8fd5bc48b39809a1bee
ifbe <ifbe@live.cn>
2016-11-23 17:14:31 +0800

    修改：     software1/bigint/bigint.c 	修改：     software1/bigint/main.c 	修改：     software1/prime/bigint.c 	新文件：   software1/prime/main.c 	新文件：   software1/prime/makefile 	删除：     software1/prime/prime.c

--- software1/bigint/main.c
+++ software1/bigint/main.c
@@ -26,6 +26,40 @@ int bigdiv(
 	u8* bbuf, int blen,
 	u8* quotient, int max1,
 	u8* remainder, int max2);
+int bigpow(
+        u8* base, int bl,
+        u8* exp, int el,
+        u8* mod, int ml,
+        u8* answer, int max,
+        u8* t1, int l1,
+        u8* t2, int l2,
+        u8* t3, int l3);
+void movsb(u8* rdi, u8* rsi, int rcx)
+{
+        int j;
+        if(rdi < rsi)
+        {
+                for(j=0;j<rcx;j++)
+                {
+                        rdi[j] = rsi[j];
+                }
+        }
+        else
+        {
+                for(;rcx>0;rcx--)
+                {
+                        rdi[rcx-1] = rsi[rcx-1];
+                }
+        }
+}
+void printbigint(u8* p, int i)
+{
+	int j;
+	if(i<=0)return;
+
+	printf("0x");
+	for(j=i-1;j>=0;j--)printf("%02x",p[j]);
+}
 
 
 
@@ -34,76 +68,112 @@ void main()
 {
 	int j;
 	int ret;
-	u8 a[16];
-	u8 b[16];
-	u8 c[16];
-	u8 d[16];
+	u8 a[32];
+	u8 b[32];
+	u8 c[32];
+	u8 d[32];
+	u8 e[32];
+	u8 f[32];
+	u8 g[32];
 	for(j=0;j<16;j++)
 	{
 		a[j] = 0xc0+j;
 		b[j] = 0xcf-j;
-		c[j] = d[j] = 0;
+		c[j] = d[j] = e[j] = f[j] = g[j] = 0;
 	}
+
+
+
 
 	//origin
 	printf("origin\n");
-	printf("	%016llx%016llx\n	%016llx%016llx\n",
-		*(u64*)(a+8),
-		*(u64*)a,
-		*(u64*)(b+8),
-		*(u64*)b
-	);
+	printbigint(a,16);
+	printf("\n");
+	printbigint(b,16);
+	printf("\n");
+
+
+
 
 	//add
-	printf("%llx + %llx\n",
-		*(u64*)a,
-		*(u64*)b
-	);
+	printbigint(a, 8);
+	printf(" + ");
+	printbigint(b, 8);
+	printf("\n");
+
+	printf("%016llx\n", (*(u64*)a) + (*(u64*)b) );
 	ret = bigadd(a, 8, b, 8, c, 9);
-	printf("	%016llx%016llx\n	%016llx%016llx\n",
-		(u64)0,
-		(*(u64*)a) + (*(u64*)b),
-		*(u64*)(c+8),
-		*(u64*)c
-	);
+
+	printbigint(c, ret);
+	printf("\n");
+
+
+
 
 	//sub
-	printf("%llx - %llx\n",
-		*(u64*)a,
-		*(u64*)b
-	);
+	printbigint(a, 8);
+	printf(" - ");
+	printbigint(b, 8);
+	printf("\n");
+
+	printf("%08llx\n", (*(u64*)a) - (*(u64*)b) );
 	ret = bigsub(a, 8, b, 8, c, 9);
-	printf("	%016llx%016llx\n	%016llx%016llx\n",
-		(u64)0,
-		(*(u64*)a) - (*(u64*)b),
-		*(u64*)(c+8),
-		*(u64*)c
-	);
+
+	printbigint(c, ret);
+	printf("\n");
+
+
+
 
 	//mul
-	printf("%llx * %llx\n",
-		*(u64*)a,
-		*(u64*)b
-	);
 	//ret = bigmul_onebyte(a, 8, b, 0, c, 100);
+	printbigint(a, 8);
+	printf(" * ");
+	printbigint(b, 8);
+	printf("\n");
+
+	printf("%016llx\n", (*(u64*)a) * (*(u64*)b) );
 	ret = bigmul(a, 8, b, 8, c, 16, d, 16);
-	printf("	%016llx%016llx\n	%016llx%016llx\n",
-		(u64)0,
-		(*(u64*)a) * (*(u64*)b),
-		*(u64*)(c+8),
-		*(u64*)c
-	);
+
+	printbigint(c, ret);
+	printf("\n");
+
+
+
 
 	//div
-	printf("%llx / %x\n",
-		*(u64*)a,
-		*(u32*)b
-	);
+	printbigint(a, 8);
+	printf(" / ");
+	printbigint(b, 4);
+	printf("\n");
+
+	printf("%016llx\n",	(*(u64*)a) / (*(u32*)b) );
 	ret = bigdiv(a, 8, b, 4, c, 16, d, 16);
-	printf("	%016llx%016llx\n	%016llx%016llx\n",
-		(u64)0,
-		(*(u64*)a) / (*(u32*)b),
-		*(u64*)(c+8),
-		*(u64*)c
+
+	printbigint(c, ret);
+	printf("\n");
+
+
+
+
+	//pow
+	printbigint(a, 16);
+	printf(" ^ ");
+	printbigint(b, 16);
+	printf(" %% ");
+	printbigint(c, 16);
+	printf("\n");
+
+	ret = bigpow(
+		a, 16,
+		b, 16,
+		c, 16,
+		d, 16,
+		e, 16,
+		f, 16,
+		g, 16
 	);
+
+	printbigint(d, ret);
+	printf("\n");
 }
---
+++ software1/prime/main.c
@@ -0,0 +1,199 @@
+#include<stdio.h>
+#include<string.h>
+#define u8 unsigned char
+#define u16 unsigned short
+#define u32 unsigned int
+#define u64 unsigned long long
+
+
+
+
+int bigadd(
+	u8* abuf, int alen,
+	u8* bbuf, int blen,
+	u8* answer, int max);
+int bigsub(
+	u8* abuf, int alen,
+	u8* bbuf, int blen,
+	u8* answer, int max);
+int bigmul(
+	u8* abuf, int alen,
+	u8* bbuf, int blen,
+	u8* answer, int max,
+	u8* temp, int rsvd);
+int bigdiv(
+	u8* abuf, int alen,
+	u8* bbuf, int blen,
+	u8* quotient, int max1,
+	u8* remainder, int max2);
+int bigpow(
+	u8* base, int bl,
+	u8* exp, int el,
+	u8* mod, int ml,
+	u8* ans, int al,
+	u8* t1, int l1,
+	u8* t2, int l2,
+	u8* t3, int l3);
+void movsb(u8* rdi, u8* rsi, int rcx)
+{
+	int j;
+	if(rdi < rsi)
+	{
+		for(j=0;j<rcx;j++)
+		{
+			rdi[j] = rsi[j];
+		}
+	}
+	else
+	{
+		for(;rcx>0;rcx--)
+		{
+			rdi[rcx-1] = rsi[rcx-1];
+		}
+	}
+}
+void printbigint(u8* p, int i)
+{
+	int j;
+	if(i<=0)return;
+
+	printf("0x");
+	for(j=i-1;j>=0;j--)printf("%02x",p[j]);
+}
+
+
+
+
+
+int prime_yes(u8* addr, int len)
+{
+	int j;
+	u32 temp;
+	u8 quotient[16];
+	u8 remainder[16];
+
+	if((addr[0]&0x1) == 0)return 0;
+	for(temp=3;temp<0xffffffff;temp+=2)
+	{
+		bigdiv(
+			addr, len,
+			(u8*)&temp, 4,
+			quotient, 16,
+			remainder, 16
+		);
+
+printf("%08x	%llx	%llx\n",
+	temp,
+	*(u64*)quotient,
+	*(u64*)remainder
+);
+/*
+		for(j=0;j<8;j++)
+		{
+			if(remainder[j] != 0)return 0;
+		}
+*/
+	}
+
+	return 1;
+}
+int hexstr2bigint(u8* p, u8* q)
+{
+	int j=0,k=0;
+	while(p[j] != 0)j++;
+	while(1)
+	{
+		//byte.low4
+		j--;
+		if(j < 0)break;
+		if(p[j] == 0)break;
+		if( (p[j] >= 'a') && (p[j] <= 'f') )
+                {
+                        q[k] = p[j] - 0x57;
+                        k++;
+                }
+                else if( (p[j] >= 0x30) && (p[j] <= 0x39) )
+		{
+			q[k] = p[j] - 0x30;
+			k++;
+		}
+		else return 0;
+
+		//byte.high4
+		j--;
+		if(j < 0)break;
+		if(p[j] == 0)break;
+		if( (p[j] >= 'a') && (p[j] <= 'f') )
+                {
+                        q[k-1] = (p[j]-0x57) << 4;
+                        k++;
+                }
+                else if( (p[j] >= 0x30) && (p[j] <= 0x39) )
+		{
+			q[k-1] += (p[j]-0x30) << 4;
+		}
+		else return 0;
+	}
+
+	return k;
+}
+
+
+
+
+void main()
+{
+/*
+	u64 buf = 0x5555555555555557;
+	int ret = prime_yes((u8*)&buf, 8);
+
+	if(ret > 0)printf("yes\n");
+	else printf("no\n");
+*/
+	int ret;
+
+	u8 base[0x1000];
+	int bl;
+	u8 exp[0x1000];
+	int el;
+	u8 mod[0x1000];
+	int ml;
+	u8 ans[0x1000];
+	int al;
+	u8 t1[0x1000];
+	int l1;
+	u8 t2[0x1000];
+	int l2;
+	u8 t3[0x1000];
+	int l3;
+
+	u8 basestr[] = "506a6546546546545646d45646c54656b1561a65461654d6546a5465a666a54654d35135135154978979e879841315351f635131a3515afafafafafaf513b5c13d13e513a51b313c548d97e98f798a79798b79879b84653135135135135135135135135135138461451664648464a545b646c546d546e54f651561647498979461321321564879877897987987533135135154967879843513513541548798751516465465465465779846565156465465465465566119511981452642626752675267567567526456435";
+	u8 expstr[] = "27952654654651657abcdefedcba982349102983410298a34b701928c370d4712a934e701a298f474a922c73a41081e278e0491c2837407102938740128937409182374098127349817230471029384710923874012479123744918237409123740918237401298374091274091287340918274198274091827340198271298475264765765413214567141";
+	u8 modstr[] = "98273491654153135159687764351313513535874687687987643315786374912803abcedabdbdbdbdcccccdabbbbbbaeeeeeeffffff2304712958712398571928374102387412098741902837491827340912734891709283749871230947123474";
+
+	bl = hexstr2bigint(basestr, base);
+	el = hexstr2bigint(expstr, exp);
+	ml = hexstr2bigint(modstr, mod);
+printf("%d,%d,%d\n",bl<<3,el<<3,ml<<3);
+
+printf("hex(pow(");
+	printbigint(base,bl);
+	printf(",");
+	printbigint(exp,el);
+	printf(",");
+	printbigint(mod,ml);
+	printf("))\n");
+
+	ret = bigpow( 
+		base, bl,
+		exp, el,
+		mod, ml,
+		ans, al,
+		t1, l1,
+		t2, l2,
+		t3, l3
+	);
+
+	printbigint(ans, ret);
+	printf("\n");
+}
