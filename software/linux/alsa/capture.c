5081c6d4d71123130d15e4331399602d776fd112
ifbe <ifbe@live.cn>
2016-12-18 21:51:38 +0800

    linux voice capture

--- software/linux/alsa/capture.c
+++ software/linux/alsa/capture.c
@@ -1,81 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <alsa/asoundlib.h>
-      
-int main (int argc, char *argv[])
+static char buffer[0x100000];
+
+void main (int argc, char *argv[])
 {
-int i;
-int err;
-short buf[128];
-snd_pcm_t *capture_handle;
-snd_pcm_hw_params_t *hw_params;
+	int i;
+	int err;
+	int buffer_frames = 441;	//10ms
+	unsigned int rate = 44100;
+	snd_pcm_t *capture_handle;
+	snd_pcm_hw_params_t *hw_params;
+	snd_pcm_format_t format = SND_PCM_FORMAT_S16_LE;
 
-if ((err = snd_pcm_open (&capture_handle, argv[1], SND_PCM_STREAM_CAPTURE, 0)) < 0) {
-fprintf (stderr, "cannot open audio device %s (%s)\n", 
- argv[1],
- snd_strerror (err));
-exit (1);
-}
-   
-if ((err = snd_pcm_hw_params_malloc (&hw_params)) < 0) {
-fprintf (stderr, "cannot allocate hardware parameter structure (%s)\n",
- snd_strerror (err));
-exit (1);
-}
- 
-if ((err = snd_pcm_hw_params_any (capture_handle, hw_params)) < 0) {
-fprintf (stderr, "cannot initialize hardware parameter structure (%s)\n",
- snd_strerror (err));
-exit (1);
-}
+	printf("@snd_pcm_open\n");
+	err = snd_pcm_open (&capture_handle, argv[1], SND_PCM_STREAM_CAPTURE, 0);
+	if (err < 0) {
+		printf("%s(%s)\n", argv[1], snd_strerror (err));
+		exit (1);
+	}
 
-if ((err = snd_pcm_hw_params_set_access (capture_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
-fprintf (stderr, "cannot set access type (%s)\n",
- snd_strerror (err));
-exit (1);
-}
+	printf("@snd_pcm_hw_params_malloc\n");
+	err = snd_pcm_hw_params_malloc (&hw_params);
+	if (err < 0) {
+		printf ("%s\n", snd_strerror (err));
+		exit (1);
+	}
 
-if ((err = snd_pcm_hw_params_set_format (capture_handle, hw_params, SND_PCM_FORMAT_S16_LE)) < 0) {
-fprintf (stderr, "cannot set sample format (%s)\n",
- snd_strerror (err));
-exit (1);
-}
+	printf("@snd_pcm_hw_params_any\n");
+	err = snd_pcm_hw_params_any (capture_handle, hw_params);
+	if (err < 0) {
+		printf ("%s\n", snd_strerror (err));
+		exit (1);
+	}
 
-err=44100;
-if ((err = snd_pcm_hw_params_set_rate_near (capture_handle, hw_params, &err, 0)) < 0) {
-fprintf (stderr, "cannot set sample rate (%s)\n",
- snd_strerror (err));
-exit (1);
-}
+	printf("snd_pcm_hw_params_set_access\n");
+	err = snd_pcm_hw_params_set_access(
+		capture_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED);
+	if (err < 0) {
+		printf ("%s\n", snd_strerror (err));
+		exit (1);
+	}
 
-if ((err = snd_pcm_hw_params_set_channels (capture_handle, hw_params, 2)) < 0) {
-fprintf (stderr, "cannot set channel count (%s)\n",
- snd_strerror (err));
-exit (1);
-}
+	printf("@snd_pcm_hw_params_set_format\n");
+	err = snd_pcm_hw_params_set_format (capture_handle, hw_params, format);
+	if (err < 0) {
+		printf ("%s\n", snd_strerror (err));
+		exit (1);
+	}
 
-if ((err = snd_pcm_hw_params (capture_handle, hw_params)) < 0) {
-fprintf (stderr, "cannot set parameters (%s)\n",
- snd_strerror (err));
-exit (1);
-}
+	printf("@snd_pcm_hw_params_set_rate_near\n");
+	err = snd_pcm_hw_params_set_rate_near (capture_handle, hw_params, &rate, 0);
+	if (err < 0) {
+		printf ("%s\n", snd_strerror (err));
+		exit (1);
+	}
 
-snd_pcm_hw_params_free (hw_params);
+	printf("@snd_pcm_hw_params_set_channels\n");
+	err = snd_pcm_hw_params_set_channels (capture_handle, hw_params, 2);
+	if (err < 0) {
+		printf ("%s\n", snd_strerror (err));
+		exit (1);
+	}
 
-if ((err = snd_pcm_prepare (capture_handle)) < 0) {
-fprintf (stderr, "cannot prepare audio interface for use (%s)\n",
- snd_strerror (err));
-exit (1);
-}
+	printf("@snd_pcm_hw_params\n");
+	err = snd_pcm_hw_params (capture_handle, hw_params);
+	if (err < 0) {
+		printf ("%s\n", snd_strerror (err));
+		exit (1);
+	}
+	snd_pcm_hw_params_free (hw_params);
 
-for (i = 0; i < 10; ++i) {
-if ((err = snd_pcm_readi (capture_handle, buf, 128)) != 128) {
-fprintf (stderr, "read from audio interface failed (%s)\n",
- snd_strerror (err));
-exit (1);
-}
-}
+	printf("@snd_pcm_hw_params\n");
+	err = snd_pcm_prepare (capture_handle);
+	if (err < 0) {
+		printf ("%s\n", snd_strerror (err));
+		exit (1);
+	}
 
-snd_pcm_close (capture_handle);
-exit (0);
+	printf("@snd_pcm_readi\n");
+	for (i = 0; i < 1000; ++i) {
+		err = snd_pcm_readi (capture_handle, buffer, buffer_frames);
+		if (err != buffer_frames) {
+			printf ("%s\n", snd_strerror (err));
+			exit (1);
+		}
+		printf("%d done\n", i);
+	}
+
+	printf("@snd_pcm_close\n");
+	snd_pcm_close (capture_handle);
 }
