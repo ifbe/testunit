90d37661fa6c099fa1fb973b3170eeee59364556
ifbe <ifbe@live.cn>
2017-01-05 13:14:46 +0800

    v4l2

--- software/linux/v4l2/v4l2.c
+++ software/linux/v4l2/v4l2.c
@@ -1,197 +1,168 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
-#include <sys/epoll.h>
-#include <getopt.h>           
-
-#include <fcntl.h>             
+#include <fcntl.h>	     
 #include <unistd.h>
 #include <errno.h>
-#include <malloc.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/time.h>
-#include <sys/mman.h>
+#include <sys/epoll.h>
 #include <sys/ioctl.h>
-
-#include <asm/types.h>         
+#include <sys/mman.h>
 #include <linux/videodev2.h>
+struct buffer{
+        void *start;
+        size_t length;
+}my[24];
 
-struct buffer {
-	void *start;
-	size_t length;
-};
 
-struct buffer *buffers=NULL;
-static unsigned int nbuffers=0;
-FILE *file_fd[24];
-static int read_frame(int fd)
+
+
+void fileout(int j, char* buf, int len)
 {
-	struct v4l2_buffer buf;
-	unsigned int i;
-	
-	memset(&buf,0,sizeof(buf));
-	buf.type=V4L2_BUF_TYPE_VIDEO_CAPTURE;
-	buf.memory=V4L2_MEMORY_MMAP;
-
-	ioctl(fd,VIDIOC_DQBUF,&buf);
-	assert(buf.index<nbuffers);
-	printf("buf.index=%d\n",buf.index);
-	fseek(file_fd[buf.index],0,SEEK_SET);
-	fwrite(buffers[buf.index].start,buffers[buf.index].length,1,file_fd[buf.index]);
-	ioctl(fd,VIDIOC_QBUF,&buf);
-
-	return buf.index;
+	char haha[32];
+	snprintf(haha, 32, "%d.jpg", j);
+	printf("(%s,%d)%x,%x,%x,%x\n",haha, len, buf[0], buf[1], buf[2], buf[3]);
+	j = open(haha, O_RDWR|O_CREAT, S_IRWXU|S_IRWXG|S_IRWXO);
+	write(j, buf, len);
+	close(j);
 }
-
-int main (int argc,char ** argv)
+int main(int argc, char* argv[])
 {
-	int fd,i;
-	int size;
-	
-	char buf[20];
-	for(i=0;i<24;i++)
+	//v4l2_open
+	int j,k;
+	int fd = open("/dev/video0",O_RDWR);	//|O_NONBLOCK);
+	if(fd <= 0)
 	{
-		sprintf(buf,"test[%d].jpg",i);
-		file_fd[i]=fopen(buf,"w");
+		printf("error@open /dev/video0\n");
+		return 0;
 	}
-	fd=open("/dev/video0",O_RDWR);
+
+	//v4l2_capability
 	struct v4l2_capability cap;
 	if(-1==ioctl(fd,VIDIOC_QUERYCAP,&cap))
 	{
 		printf("VIDIOC_QUERYCAP error\n");
 		exit(1);
 	}
-	printf("Capability Informations:\n");
-    	printf(" driver: %s\n", cap.driver);
-    	printf(" card: %s\n", cap.card);
-    	printf(" bus_info: %s\n", cap.bus_info);
-    	printf(" version: %08X\n", cap.version);
-    	printf(" capabilities: %08X\n", cap.capabilities);
+	printf("v4l2_capability:\n");
+    	printf("	driver: %s\n", cap.driver);
+    	printf("	card: %s\n", cap.card);
+    	printf("	bus_info: %s\n", cap.bus_info);
+    	printf("	version: %08X\n", cap.version);
+    	printf("	capabilities: %08X\n", cap.capabilities);
 
-	struct v4l2_fmtdesc fmtdesc; 
-	fmtdesc.index=0; 
-	fmtdesc.type=V4L2_BUF_TYPE_VIDEO_CAPTURE; 
-	printf("Support format:\n");
-
-	while(ioctl(fd, VIDIOC_ENUM_FMT, &fmtdesc) != -1)
+	//v4l2_fmtdesc
+	struct v4l2_fmtdesc desc; 
+	desc.type=V4L2_BUF_TYPE_VIDEO_CAPTURE; 
+	desc.index=0; 
+	printf("v4l2_fmtdesc:\n");
+	while(ioctl(fd, VIDIOC_ENUM_FMT, &desc) != -1)
 	{
-		printf("\t%d.%s\n",fmtdesc.index+1,fmtdesc.description);
-		fmtdesc.index++;
+		printf("	%s\n", desc.description);
+		desc.index++;
 	}
-////////set format/////
+	if (desc.index == 0)
+	{
+		perror("VIDIOC_ENUM_FMT");
+	}
+
+	//v4l2_format
 	struct v4l2_format fmt;
-	memset(&fmt,0,sizeof(fmt));
-	
 	fmt.type		= V4L2_BUF_TYPE_VIDEO_CAPTURE;
 	fmt.fmt.pix.width	= 640;
 	fmt.fmt.pix.height	= 480;
 	fmt.fmt.pix.pixelformat	= V4L2_PIX_FMT_MJPEG;
 	fmt.fmt.pix.field	= V4L2_FIELD_INTERLACED;	
-	if(-1==ioctl(fd,VIDIOC_S_FMT,&fmt))
+	if(ioctl(fd,VIDIOC_S_FMT,&fmt) == -1)
 	{
 		printf("VIDIOC_S_FMT error\n");
 		exit(1);
 	}
-/////////////
-//////////////
-	struct v4l2_requestbuffers req;
-	memset(&req,0,sizeof(req));
 
+	//v4l2_requestbuffers
+	struct v4l2_requestbuffers req;
 	req.count	= 24;
 	req.type	= V4L2_BUF_TYPE_VIDEO_CAPTURE;
-	req.memory	= V4L2_MEMORY_MMAP;
-	if(-1==ioctl(fd,VIDIOC_REQBUFS,&req))
+	req.memory	= V4L2_MEMORY_MMAP;	//V4L2_MEMORY_USERPTR;
+	if(ioctl(fd,VIDIOC_REQBUFS,&req) == -1)
 	{
 		printf("VIDIOC_REQBUFS error\n");
 		exit(1);
 	}
-//////////////////
-//////////////////
-	buffers=calloc(req.count,sizeof(struct buffer));
-	for(nbuffers=0;nbuffers<req.count;++nbuffers)
-	{
-		struct v4l2_buffer buf;
-		memset(&buf,0,sizeof(buf));
-		
-		buf.type	= V4L2_BUF_TYPE_VIDEO_CAPTURE;
-		buf.memory	= V4L2_MEMORY_MMAP;
-		buf.index	= nbuffers;
 
-		if(-1==ioctl(fd,VIDIOC_QUERYBUF,&buf))
+	//prepare
+	struct v4l2_buffer buf;
+	for(j=0;j<24;j++)
+	{
+		buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+		buf.memory = V4L2_MEMORY_MMAP;
+		buf.index = j;
+		if(ioctl(fd,VIDIOC_QUERYBUF,&buf) == -1)
 		{
-			printf("VIDIOC_QUERYBUF error\n");
-			exit(1);
-		}		
-		buffers[nbuffers].length = buf.length;
-		buffers[nbuffers].start = mmap(NULL,
-						buf.length,
-						PROT_READ|PROT_WRITE,
-						MAP_SHARED,
-						fd,
-						buf.m.offset
-						);
-		if(MAP_FAILED == buffers[nbuffers].start)
-		{
-			printf("mmap failed\n");
+			printf("VIDIOC_QUERYBUF\n");
 			exit(1);
 		}
 
+		my[j].length = buf.length;
+		my[j].start = mmap(
+			NULL,
+			buf.length,
+			PROT_READ|PROT_WRITE,
+			MAP_SHARED,
+			fd,
+			buf.m.offset
+		);
 
-		if(-1==ioctl(fd,VIDIOC_QBUF,&buf))
+		if(my[j].start == MAP_FAILED)
 		{
-			printf("VIDIOC_QBUF error\n");
+			printf("fail@mmap\n");
 			exit(1);
 		}
-
-	}	
-
-	int type=V4L2_BUF_TYPE_VIDEO_CAPTURE;
-	if(-1==ioctl(fd,VIDIOC_STREAMON,&type))
-	{
-		printf("VIDIOC_STREAM error\n");
-		exit(1);
-	}
-	
-
-
-	while(1)
-	{
-		int epfd,nfds,ret;
-		struct epoll_event ev,events;
-		epfd=epoll_create(256);
-	
-		ev.data.fd=fd;
-		ev.events = EPOLLIN | EPOLLET;
-		epoll_ctl(epfd,EPOLL_CTL_ADD,fd,&ev);	
-		nfds = epoll_wait(epfd, &events, 1, -1);
-		if(nfds==-1)
-		{	
-			printf("epoll error\n");
-			exit(1);
-		}
-		ret=read_frame(fd);
-		if(ret==23)
+		if(ioctl(fd,VIDIOC_QBUF,&buf) == -1)
 		{
-			break;
+			printf("VIDEOC_QBUF\n");
+			exit(1);
 		}
 	}
-	
-	unmap:
-	for (i = 0; i < nbuffers; ++i)
+
+	//start
+	j = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        if(ioctl(fd, VIDIOC_STREAMON, &j) == -1)
+        {
+                printf("error@ON\n");
+                exit(1);
+        }
+
+	//record
+	int epfd;
+	struct epoll_event ev;
+	epfd=epoll_create(256);
+	ev.data.fd=fd;
+	ev.events = EPOLLIN | EPOLLET;
+	epoll_ctl(epfd,EPOLL_CTL_ADD,fd,&ev);
+	for(j=0;j<24;j++)
 	{
-		fclose (file_fd[i]);
-		if (-1 == munmap (buffers[i].start, buffers[i].length))
-	    		printf ("munmap error");
-	}	
-	close (fd);
-	
-	exit (EXIT_SUCCESS);
-	return 0;
+		//!!!!!!!!!!!!!!must take out ontime!!!!!!!!!!!!!!
+                k = epoll_wait(epfd, &ev, 1, -1);
+                if(k == -1)
+                {
+                        printf("epoll error\n");
+                        exit(1);
+                }
 
+		//
+		buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+		buf.memory = V4L2_MEMORY_MMAP;
+		ioctl(fd, VIDIOC_DQBUF, my[j].start);
+		fileout(j, my[j].start, my[j].length);
+		//ioctl(fd, VIDIOC_QBUF, &buf);
+	}
 
+	//stop
+	for(j=0;j<24;j++)
+        {
+		munmap(my[j].start, my[j].length);
+        }
+
+	//v4l2_release
+	close(fd);
 }
-
-
-
