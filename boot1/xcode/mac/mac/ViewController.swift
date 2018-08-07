16ff8bed1f4d38aa174a6e75ba197fb0650f53c3
ifbe <ifbe@live.cn>
2018-08-07 22:49:56 +0800

    reorganize

---
+++ boot1/xcode/mac/mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mac
+//
+//  Created by 史蒙健 on 2018/7/25.
+//  Copyright © 2018年 史蒙健. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ boot1/xcode/metal/metal/metal.metal
@@ -0,0 +1,12 @@
+//
+//  metal.metal
+//  metal
+//
+//  Created by 史蒙健 on 2018/8/7.
+//  Copyright © 2018年 史蒙健. All rights reserved.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+
