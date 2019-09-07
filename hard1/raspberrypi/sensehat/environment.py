0ecea6094775b3e9bf2885ae62dea1d104b7cb6e
ifbe <ifbe@live.cn>
2019-09-07 15:16:08 +0800

    sensehat

---
+++ hard1/raspberrypi/sensehat/environment.py
@@ -0,0 +1,34 @@
+from sense_hat import SenseHat
+sense = SenseHat()
+
+# Define the colours red and green
+red = (255, 0, 0)
+green = (0, 255, 0)
+black = (0, 0, 0)
+
+while True:
+
+  # Take readings from all three sensors
+  t = sense.get_temperature()
+  p = sense.get_pressure()
+  h = sense.get_humidity()
+
+  # Round the values to one decimal place
+  t = round(t, 1)
+  p = round(p, 1)
+  h = round(h, 1)
+  
+  # Create the message
+  # str() converts the value to a string so it can be concatenated
+  message = "T=" + str(t) + " P=" + str(p) + " H=" + str(h)
+  
+  if t > 18.3 and t < 26.7:
+    bg = green
+  else:
+    bg = red
+  
+  bg = black
+
+  # Display the scrolling message
+  sense.set_rotation(90)
+  sense.show_message(message, scroll_speed=0.1, back_colour=bg)
---
+++ hard1/raspberrypi/sensehat/helloworld.py
@@ -0,0 +1,4 @@
+from sense_hat import SenseHat
+sense = SenseHat()
+sense.set_rotation(90)
+sense.show_message("Hello world")
