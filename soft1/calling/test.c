b8693abb166c2823d8eede728268a5f7d45ec860
ifbe <ifbe@live.cn>
2021-12-27 23:34:51 +0800

    soft1/calling

---
+++ soft1/calling/test.c
@@ -0,0 +1,47 @@
+void test()
+{
+}
+void test1(int a)
+{
+}
+void test2(int a,int b)
+{
+}
+void test3(int a,int b,int c)
+{
+}
+void test4(int a,int b,int c,int d)
+{
+}
+void test5(int a,int b,int c,int d,int e)
+{
+}
+void test6(int a,int b,int c,int d,int e,int f)
+{
+}
+void test7(int a,int b,int c,int d,int e,int f,int g)
+{
+}
+void test8(int a,int b,int c,int d,int e,int f,int g,int h)
+{
+}
+void test9(int a,int b,int c,int d,int e,int f,int g,int h,int i)
+{
+}
+void testa(int a,int b,int c,int d,int e,int f,int g,int h,int i,int j)
+{
+}
+void main()
+{
+	test();
+	test1(0x10);
+	test2(0x10,1);
+	test3(0x10,1,2);
+	test4(0x10,1,2,3);
+	test5(0x10,1,2,3,4);
+	test6(0x10,1,2,3,4,5);
+	test7(0x10,1,2,3,4,5,6);
+	test8(0x10,1,2,3,4,5,6,7);
+	test9(0x10,1,2,3,4,5,6,7,8);
+	testa(0x10,1,2,3,4,5,6,7,8,9);
+}
