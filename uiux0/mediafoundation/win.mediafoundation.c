61da50cdb1e8a1429ff7821d91076a13956b8481
ifbe <ifbe@live.cn>
2023-02-19 21:50:05 +0800

    win.mediafoundation

--- uiux0/mediafoundation/win.mediafoundation.c
+++ uiux0/mediafoundation/win.mediafoundation.c
@@ -16,8 +16,6 @@ IMFSourceReader* reader = NULL;
 IMFMediaType* mediatype = NULL;
 GUID subtype;
 //
-u32 width;
-u32 height;
 u32 camcount = 0;
 int exitflag;
 
@@ -41,53 +39,227 @@ void guid2str(GUID* guid, char* str){
 	}
 }
 char* mf_subtype2format(GUID* guid, char* str){
-	if(IsEqualGUID(subtype, MFVideoFormat_AI44)){
-		return "AI44";
+	char const * c = 0;		//solve warning: ISO C++ forbids converting a string constant to 'char*'
+	if(0 == guid){
+		return 0;
 	}
-	else if(IsEqualGUID(subtype, MFVideoFormat_AYUV)){
-		return "AYUV";
+
+	//Uncompressed RGB Formats
+	else if(IsEqualGUID(*guid, MFVideoFormat_RGB8)){
+		c = "RGB8";
 	}
-	else if(IsEqualGUID(subtype, MFVideoFormat_I420)){
-		return "I420";
+	else if(IsEqualGUID(*guid, MFVideoFormat_RGB555)){
+		c = "RGB555";
 	}
-	else if(IsEqualGUID(subtype, MFVideoFormat_IYUV)){
-		return "IYUV";
+	else if(IsEqualGUID(*guid, MFVideoFormat_RGB565)){
+		c = "RGB565";
 	}
-	else if(IsEqualGUID(subtype, MFVideoFormat_NV11)){
-		return "NV11";
+	else if(IsEqualGUID(*guid, MFVideoFormat_RGB24)){
+		c = "RGB24";
 	}
-	else if(IsEqualGUID(subtype, MFVideoFormat_NV12)){
-		return "NV12";
+	else if(IsEqualGUID(*guid, MFVideoFormat_RGB32)){
+		c = "RGB32";
 	}
-	else if(IsEqualGUID(subtype, MFVideoFormat_NV21)){
-		return "NV21";
+	else if(IsEqualGUID(*guid, MFVideoFormat_ARGB32)){
+		c = "ARGB32";
 	}
-	else if(IsEqualGUID(subtype, MFVideoFormat_UYVY)){
-		return "UYVY";
+	else if(IsEqualGUID(*guid, MFVideoFormat_A2R10G10B10)){
+		c = "A2R10G10B10";
 	}
-	else if(IsEqualGUID(subtype, MFVideoFormat_Y41P)){
-		return "Y41P";
+	else if(IsEqualGUID(*guid, MFVideoFormat_A16B16G16R16F)){
+		c = "A16B16G16R16F";
 	}
-	else if(IsEqualGUID(subtype, MFVideoFormat_Y41T)){
-		return "Y41T";
+
+	//YUV Formats: 8-Bit and Palettized
+	else if(IsEqualGUID(*guid, MFVideoFormat_AI44)){
+		c = "AI44";
 	}
-	else if(IsEqualGUID(subtype, MFVideoFormat_Y42T)){
-		return "Y42T";
+	else if(IsEqualGUID(*guid, MFVideoFormat_AYUV)){
+		c = "AYUV";
 	}
-	else if(IsEqualGUID(subtype, MFVideoFormat_YUY2)){
-		return "YUV2";
+	else if(IsEqualGUID(*guid, MFVideoFormat_I420)){
+		c = "I420";
 	}
-	else if(IsEqualGUID(subtype, MFVideoFormat_YVU9)){
-		return "YVU9";
+	else if(IsEqualGUID(*guid, MFVideoFormat_IYUV)){
+		c = "IYUV";
 	}
-	else if(IsEqualGUID(subtype, MFVideoFormat_YV12)){
-		return "YV12";
+	else if(IsEqualGUID(*guid, MFVideoFormat_NV11)){
+		c = "NV11";
 	}
-	else if(IsEqualGUID(subtype, MFVideoFormat_YVYU)){
-		return "YVYU";
+	else if(IsEqualGUID(*guid, MFVideoFormat_NV12)){
+		c = "NV12";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_NV21)){
+		c = "NV21";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_UYVY)){
+		c = "UYVY";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_Y41P)){
+		c = "Y41P";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_Y41T)){
+		c = "Y41T";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_Y42T)){
+		c = "Y42T";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_YUY2)){
+		c = "YUV2";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_YVU9)){
+		c = "YVU9";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_YV12)){
+		c = "YV12";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_YVYU)){
+		c = "YVYU";
+	}
+
+	//YUV Formats: 10-Bit and 16-Bit
+	else if(IsEqualGUID(*guid, MFVideoFormat_P010)){
+		c = "P010";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_P016)){
+		c = "P016";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_P210)){
+		c = "P210";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_P216)){
+		c = "P216";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_v210)){
+		c = "v210";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_v216)){
+		c = "v216";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_v410)){
+		c = "v410";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_Y210)){
+		c = "Y210";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_Y216)){
+		c = "Y216";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_Y410)){
+		c = "Y410";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_Y416)){
+		c = "Y416";
+	}
+
+	//Luminance and Depth Formats
+	else if(IsEqualGUID(*guid, MFVideoFormat_L8)){
+		c = "L8";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_L16)){
+		c = "L16";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_D16)){
+		c = "D16";
+	}
+
+	//Encoded Video Types
+	else if(IsEqualGUID(*guid, MFVideoFormat_DV25)){
+		c = "DV25";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_DV50)){
+		c = "DV50";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_DVC)){
+		c = "DVC";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_DVH1)){
+		c = "DVH1";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_DVHD)){
+		c = "DVHD";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_DVSD)){
+		c = "DVSD";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_DVSL)){
+		c = "DVSL";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_H263)){
+		c = "H263";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_H264)){
+		c = "H264";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_H265)){
+		c = "H265";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_H264_ES)){
+		c = "H264_ES";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_HEVC)){
+		c = "HEVC";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_HEVC_ES)){
+		c = "HEVC_ES";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_M4S2)){
+		c = "M4S2";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_MJPG)){
+		c = "MJPG";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_MP43)){
+		c = "MP43";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_MP4S)){
+		c = "MP4S";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_MP4V)){
+		c = "MP4V";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_MPEG2)){
+		c = "MPEG2";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_VP80)){
+		c = "VP80";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_VP90)){
+		c = "VP90";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_MPG1)){
+		c = "MPG1";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_MSS1)){
+		c = "MSS1";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_MSS2)){
+		c = "MSS2";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_WMV1)){
+		c = "WMV1";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_WMV2)){
+		c = "WMV2";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_WMV3)){
+		c = "WMV3";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_WVC1)){
+		c = "WVC1";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_420O)){
+		c = "420O";
+	}
+	else if(IsEqualGUID(*guid, MFVideoFormat_AV1)){
+		c = "AV1";
+	}
+
+	if(c){
+		return (char*)c;
 	}
 	else{
-		guid2str(&subtype, str);
+		guid2str(guid, str);
 		return str;
 	}
 }
@@ -116,6 +288,14 @@ int mf_enum(IMFMediaSource*){
 	for(int j=0;j<desccount;j++){
 		hr = desc->GetStreamDescriptorByIndex(j, &pfSelected, &streamdesc);
 
+		wchar_t wstr[64];
+		UINT32 wlen;
+		streamdesc->GetString(MF_SD_STREAM_NAME, wstr, 64, &wlen);
+
+		char str[64];
+		wstr2str(wstr,wlen, str,64);
+		printf("stream%d: %s\n", j, str);
+
 		IMFMediaTypeHandler* hand;
 		hr = streamdesc->GetMediaTypeHandler(&hand);
 
@@ -123,11 +303,16 @@ int mf_enum(IMFMediaSource*){
 		hr = hand->GetMediaTypeCount(&cnt2);
 		for(int k=0;k<cnt2;k++){
 			hr = hand->GetMediaTypeByIndex(k, &mediatype);
-			hr = MFGetAttributeSize(mediatype, MF_MT_FRAME_SIZE, &width, &height);
 			hr = mediatype->GetGUID(MF_MT_SUBTYPE, &subtype);
+			u32 width;
+			u32 height;
+			hr = MFGetAttributeSize(mediatype, MF_MT_FRAME_SIZE, &width, &height);
+			u32 numerator;
+			u32 denominator;
+			hr = MFGetAttributeSize(mediatype, MF_MT_FRAME_RATE, &numerator, &denominator);
 
 			char str[64];
-			printf("%d: width=%d,height=%d,format=%.32s\n", k, width, height, mf_subtype2format(&subtype, str));
+			printf("%d: width=%d,height=%d,fps=%f,format=%.32s\n", k, width, height, (float)numerator/denominator, mf_subtype2format(&subtype, str));
 		}
 	}
 	return 0;
@@ -149,6 +334,8 @@ int mf_select(IMFMediaSource*){
 		printf("8\n");
 	}
 
+	u32 width;
+	u32 height;
 	hr = MFGetAttributeSize(mediatype, MF_MT_FRAME_SIZE, &width, &height);
 	if(FAILED(hr)){
 		printf("9\n");
