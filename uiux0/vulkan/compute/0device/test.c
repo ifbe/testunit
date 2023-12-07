d81d13b2aad734f6216f37bd876f22430d61ed70
ifbe <ifbe@live.cn>
2023-12-07 19:45:28 +0800

    makefile for vulkan compute(version 1.3.268)

--- uiux0/vulkan/compute/0device/test.c
+++ uiux0/vulkan/compute/0device/test.c
@@ -5,6 +5,7 @@
 void* vulkan_device_create(int, void*);
 void vulkan_device_delete(void*);
 void vulkan_physicaldevice_logicdevice(VkPhysicalDevice* pdev, VkDevice* ldev);
+void vulkan_computequeue_computepool(VkQueue* queue, VkCommandPool* pool);
 
 
 
@@ -24,6 +25,8 @@ void vulkan_myctx_create()
 	if(0 == physicaldevice)return;
 
 	vulkan_physicaldevice_logicdevice(&physicaldevice, &logicaldevice);
+
+	vulkan_computequeue_computepool(&computeQueue, &computePool);
 }
 void vulkan_myctx_delete()
 {
